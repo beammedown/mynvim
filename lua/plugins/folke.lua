a7f3a0c0ce135310cc738a7650fa1e5097469495
beammedown <gaston@liepach.eu>
2025-05-13 22:43:21 -0600

    added telescope context

--- lua/plugins/folke.lua
+++ lua/plugins/folke.lua
@@ -16,8 +16,8 @@ return {
     opts = {},
   -- stylua: ignore
   keys = {
-    { '<C-s>', mode = { 'n', 'x', 'o' }, function() require('flash').jump() end, desc = 'Flash' },
-    { '<C-S>', mode = { 'n', 'x', 'o' }, function() require('flash').treesitter() end, desc = 'Flash Treesitter' },
+    { '<leader>g', mode = { 'n', 'x', 'o' }, function() require('flash').jump() end, desc = 'Flash' },
+    { '<leader>G', mode = { 'n', 'x', 'o' }, function() require('flash').treesitter() end, desc = 'Flash Treesitter' },
     { 'r', mode = 'o', function() require('flash').remote() end, desc = 'Remote Flash' },
     { 'R', mode = { 'o', 'x' }, function() require('flash').treesitter_search() end, desc = 'Treesitter Search' },
     { '<c-s>', mode = { 'c' }, function() require('flash').toggle() end, desc = 'Toggle Flash Search' },
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,16 +5,16 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
 
     config = function()
-      require('telescope').setup({
+      require('telescope').setup {
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
           },
         },
-      })
-      require('telescope').load_extension('ui-select')
+      }
+      require('telescope').load_extension 'ui-select'
 
-      local builtin = require('telescope.builtin')
+      local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>sd', builtin.lsp_document_symbols, { desc = '[S]earch [D]ocument symbols' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch [G]rep' })
@@ -22,12 +22,30 @@ return {
       vim.keymap.set('n', '<leader>sq', builtin.diagnostics, { desc = '[S]earch diagnostics' })
       vim.keymap.set('n', '<leader>/', function()
         -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-        builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown({
+        builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
           winblend = 10,
           previewer = false,
-        }))
+        })
       end, { desc = '[/] Fuzzily search in current buffer' })
     end,
   },
   { 'nvim-telescope/telescope-ui-select.nvim' },
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+    enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+    opts = {
+      multiwindow = false, -- Enable multiwindow support.
+      max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+      min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+      line_numbers = true,
+      multiline_threshold = 20, -- Maximum number of lines to show for a single context
+      trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+      mode = 'cursor', -- Line used to calculate context. Choices: 'cursor', 'topline'
+      -- Separator between context and content. Should be a single character string, like '-'.
+      -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+      separator = nil,
+      zindex = 20, -- The Z-index of the context window
+      on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+    },
+  },
 }
